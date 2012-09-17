a033867527fc5a6fcf42398bf2254393cb1e2eeb
ltiao-git --version <ctia193@cse.unsw.EDU.AU>
2012-09-17 17:51:39 +1000

    cleaned up junk

--- lab08/DisplayQuoridorBoard.java
+++ lab08/DisplayQuoridorBoard.java
@@ -1,13 +1,8 @@
 package lab08;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
-
 public class DisplayQuoridorBoard implements Display {
 
 	static final int BOARD_SIZE = 9;
-	//private List <String> moves = new LinkedList<String>();
 	
 	@Override
 	public void display(String moves) {
@@ -16,60 +11,12 @@ public class DisplayQuoridorBoard implements Display {
 				print (i,j);
 			}
 		}
-		/*
-		parseString (moves);
-		for (int i = 0 ; i < 2*BOARD_SIZE+1 ; i++) {
-			if (i%2 == 0) { 
-				printHorizontal ();
-			} 
-			else {
-				printVertical ();	
-			}
-		}
-		*/
-	}
-	
-	private void parseString (String moves) {
-		 StringTokenizer st = new StringTokenizer(moves);
-		 while (st.hasMoreTokens()) {
-	         System.out.println(st.nextToken());
-	     }
-		 //System.out.println(moves);
-	}
-	
-	private void printHorizontal() {
-		for (int i = 0 ; i < 2*BOARD_SIZE+1 ; i++) {
-			if (i%2 == 0) {
-				System.out.print("+");
-				//System.out.print(i);
-			}
-			else {
-				System.out.print("---");
-			}
-			
-		}
-		System.out.println();
-	}
-	
-	private void printVertical() {
-		for (int i = 0 ; i < 2*BOARD_SIZE+1 ; i++) {
-			if (i%2 == 0) {
-				System.out.print("|");
-				//System.out.print(i);
-			}
-			else {
-				System.out.print("   ");
-			}
-		}
-		System.out.println();
 	}
 	
 	private void print (int i, int j) {
 		if ((i+j)%2 == 0) {
 			if (j%2==0) {
 				System.out.print ("+");
-				if (j==2*BOARD_SIZE) 
-					System.out.println ();
 			}				
 			else
 				System.out.print ("   ");
@@ -78,11 +25,11 @@ public class DisplayQuoridorBoard implements Display {
 				System.out.print ("---");
 			else {
 				System.out.print ("|");
-				if (j==2*BOARD_SIZE) 
-					System.out.println ();
 			}
 				
 		}
+		if (j==2*BOARD_SIZE) 
+			System.out.println ();
 	}
 
 }
