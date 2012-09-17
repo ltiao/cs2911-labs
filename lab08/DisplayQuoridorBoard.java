e686c1779d3a6dfb3ef80f6a63190c1838171c5d
ltiao-git --version <ctia193@cse.unsw.EDU.AU>
2012-09-17 18:17:05 +1000

    did some prettification

--- lab08/DisplayQuoridorBoard.java
+++ lab08/DisplayQuoridorBoard.java
@@ -6,8 +6,19 @@ public class DisplayQuoridorBoard implements Display {
 	
 	@Override
 	public void display(String moves) {
+		System.out.print("   ");
+		for (char c = 'a' ; c < 'j' ; c++) {
+			System.out.print(c+"   ");
+		}
+		System.out.println ();
 		for (int i = 0 ; i < 2*BOARD_SIZE+1 ; i++) {
 			for (int j = 0 ; j < 2*BOARD_SIZE+1 ; j++) {
+				if (j == 0) {		
+					if (i%2==0)
+						System.out.print(" ");
+					else
+						System.out.print((i+1)/2);
+				}
 				print (i,j);
 			}
 		}
@@ -15,20 +26,20 @@ public class DisplayQuoridorBoard implements Display {
 	
 	private void print (int i, int j) {
 		if ((i+j)%2 == 0) {
-			if (j%2==0) {
+			if (j%2 == 0) {
 				System.out.print ("+");
 			}				
 			else
 				System.out.print ("   ");
 		} else {
-			if (i%2==0)
+			if (i%2 == 0)
 				System.out.print ("---");
 			else {
 				System.out.print ("|");
 			}
 				
 		}
-		if (j==2*BOARD_SIZE) 
+		if (j == 2*BOARD_SIZE) 
 			System.out.println ();
 	}
 
