62dac00cadab854d7ad7e0ecd727c6371b2d38e6
ltiao-git --version <ctia193@cse.unsw.EDU.AU>
2012-09-18 13:54:56 +1000

    test

--- lab08/BoardTest.java
+++ lab08/BoardTest.java
@@ -14,7 +14,7 @@ public class BoardTest {
 
 	@Test
 	public void test() {
-		board.display("e8 e2 e7 e3 e6 e4 d5 e6 d4 e7 c4 f8 c3 g8");
+		board.display("e8 e2 e7 e3 e6 e4 d5 e6 d4 e7 c4 f8 g2 i8");
 	}
 
 }
