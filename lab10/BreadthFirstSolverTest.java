0da5e9418440214d661707ec581d268371d8fe45
Louis Tiao <louistiao@gmail.com>
2012-09-25 21:59:38 +1000

    uh huh

---
+++ lab10/BreadthFirstSolverTest.java
@@ -0,0 +1,24 @@
+package lab10;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BreadthFirstSolverTest {
+
+	BreadthFirstSolver sbs = new BreadthFirstSolver();
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testNeighbors() {
+		for (int[] e:sbs.neighbors(new int[] { 1,2,0,3,4,5,6,7,8 })) {
+			for (int i = 0 ; i < e.length ; i++) {
+				System.out.print(e[i]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
