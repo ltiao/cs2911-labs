e0d57ac57b4c47a2427a4939049249f07deb9fce
Louis Tiao <louistiao@gmail.com>
2012-10-08 01:46:15 +1100

    using hashmap as marker. doubled the speed. can do better. muahahah

--- lab10/BreadthFirstSolver.java
+++ lab10/BreadthFirstSolver.java
@@ -1,6 +1,7 @@
 package lab10;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -14,31 +15,25 @@ public class BreadthFirstSolver implements SlidingBlockSolver {
 		Queue<int[]> q = new LinkedList<int[]>();
 		q.add(start);
 		
-		List <int[]> marker = new LinkedList<int[]>();
-		marker.add(start);
-			
-		int i = 0;
+		//List <int[]> marker = new LinkedList<int[]>();
+		//marker.add(start);
+		
+		HashMap <PuzzleConfiguration,Boolean> marked = new HashMap<PuzzleConfiguration,Boolean>();
+		marked.put(new PuzzleConfiguration(start), true);
 
-		int[] t;
 		while (!q.isEmpty()) {
-			System.out.println(i++);
-			t = q.poll();
+			int[] t = q.poll();
 			printPuzzle(t);
 			if (Arrays.equals(t, goal)) {
+				System.out.println(q.size());
 				return goal;
 			}
-			for (int[] o:neighbors(t)) {
-				boolean contains = false;
-				for (int[] e:marker) {
-					if (Arrays.equals(e, o)) {
-						contains = true;
-						break;
-					}
-				}
-				if (!contains) {
-					marker.add(o);
+			for (Integer e:incidentEdges(t)) {
+				int[] o = transposition(t,e,zeroIndex(t));
+				PuzzleConfiguration temp = new PuzzleConfiguration(o);
+				if (!marked.containsKey(temp)) {
+					marked.put(temp, true);
 					q.add(o);
-					//i++;
 				}
 			}
 		}
@@ -46,11 +41,16 @@ public class BreadthFirstSolver implements SlidingBlockSolver {
 		return null;
 	}
 	
+	public int zeroIndex(int[] v) {
+		int zeroIndex;
+		for (zeroIndex = 0; zeroIndex < v.length && v[zeroIndex] != 0; zeroIndex++);
+		return zeroIndex;
+	}
+	
 	public List<int[]> neighbors (int[] v) {
 		LinkedList<int[]> neighbors = new LinkedList<int[]>();
 		int n = (int) Math.sqrt(v.length);
-		int zeroIndex;
-		for (zeroIndex = 0; zeroIndex < v.length && v[zeroIndex] != 0; zeroIndex++);
+		int zeroIndex = zeroIndex(v);
 		for (int d = -1 ; d < 2 ; d++) {
 			if (d != 0) {
 				// Check the column displacement will not alter the row
@@ -64,20 +64,40 @@ public class BreadthFirstSolver implements SlidingBlockSolver {
 		}
 		return neighbors;
 	}
+	
+	public List<Integer> incidentEdges (int[] v) {
+		LinkedList<Integer> incidentEdges = new LinkedList<Integer>();
+		int n = (int) Math.sqrt(v.length);
+		int zeroIndex;
+		for (zeroIndex = 0; zeroIndex < v.length && v[zeroIndex] != 0; zeroIndex++);
+		for (int d = -1 ; d < 2 ; d++) {
+			if (d != 0) {
+				// Check the column displacement will not alter the row
+				if (zeroIndex+d >= 0 && (zeroIndex+d)/n == zeroIndex/n) {
+					incidentEdges.add(zeroIndex+d);
+				}
+				if (zeroIndex+d*n >= 0 && zeroIndex+d*n < v.length) {
+					incidentEdges.add(zeroIndex+d*n);
+				}
+			}
+		}
+		return incidentEdges;
+	}
+
 
 	public void printPuzzle (int[] array) {
 		int n = (int) Math.sqrt(array.length);
 		for (int i = 0; i < array.length; i++) {
 			System.out.format("%2d ",array[i]);
-			if ((i+1)/n != i/n) {
+			if ((i+1)/n > i/n)
 				System.out.println();
-			}
 		}
 		System.out.println();
 	}
 	
 	public int[] transposition (int[] array, int i, int j) {
-		int[] copy = Arrays.copyOf(array, array.length);
+		//int[] copy = Arrays.copyOf(array, array.length);
+		int[] copy = array.clone();
 		int temp = copy[i];
 		copy[i] = copy[j];
 		copy[j] = temp;
