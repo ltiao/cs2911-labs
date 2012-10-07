38e5f42236aaacc05a05103c86afa964b6df3bf7
Louis Tiao <louistiao@gmail.com>
2012-10-07 22:19:56 +1100

    back in the game woot woot!

--- lab10/BreadthFirstSolver.java
+++ lab10/BreadthFirstSolver.java
@@ -3,6 +3,7 @@ package lab10;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BreadthFirstSolver implements SlidingBlockSolver {
 
@@ -10,34 +11,65 @@ public class BreadthFirstSolver implements SlidingBlockSolver {
 	public int[] solve(int[] start, int[] goal, int maxMoves) {
 		if (Arrays.equals(start, goal))
 			return new int[0]; // Returns empty array if start state is already goal state
+		
+		Queue<int[]> q = new LinkedList<int[]>();
+		int[] solution = new int[maxMoves];
+		int n = (int) Math.sqrt(start.length);
+		q.add(start);
+		
+		for (int[] e: q) {
+			if (Arrays.equals(e,goal)) {
+				
+			}
+		}
 		return null;
 	}
-
-	public List<int[]> neighbors (int[] puzzle) {
+	
+	public List<int[]> neighbors (int[] v) {
 		LinkedList<int[]> neighbors = new LinkedList<int[]>();
-		int n = (int) Math.sqrt(puzzle.length);
+		int n = (int) Math.sqrt(v.length);
 		int zeroIndex;
-		for (zeroIndex = 0; zeroIndex < puzzle.length && puzzle[zeroIndex] != 0; zeroIndex++);
-		//System.out.println(zeroIndex);
+		for (zeroIndex = 0; zeroIndex < v.length && v[zeroIndex] != 0; zeroIndex++);
 		for (int d = -1 ; d < 2 ; d++) {
 			if (d != 0) {
-				if (zeroIndex+d >= 0 && zeroIndex+d < puzzle.length-1) {
-					System.out.println(zeroIndex+d);
-					neighbors.add(transposition(puzzle, zeroIndex, zeroIndex+d));
+				// Check the column displacement will not alter the row
+				if (zeroIndex+d >= 0 && (zeroIndex+d)/n == zeroIndex/n) {
+					//System.out.println(zeroIndex+d);
+					//System.out.println(Arrays.toString(v));
+					//System.out.println(Arrays.toString(transposition(v, zeroIndex, zeroIndex+d)));
+					neighbors.add(transposition(v, zeroIndex, zeroIndex+d));
 				}
-				if (zeroIndex+d*n >= 0 && zeroIndex+d*n < puzzle.length-1) {
-					System.out.println(zeroIndex+d*n);
-					neighbors.add(transposition(puzzle, zeroIndex, zeroIndex+d*n));
+				if (zeroIndex+d*n >= 0 && zeroIndex+d*n < v.length) {
+					//System.out.println(zeroIndex+d*n);
+					neighbors.add(transposition(v, zeroIndex, zeroIndex+d*n));
 				}
 			}
 		}
+		/*
+		int[][] a = new int[n][n];
+		for (int i = 0; i < v.length; i++) {
+			if (v[i]==0)
+				zeroIndex = i;
+			a[i/n][i%n] = v[i];
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				v[n*i+j]=a[i][j];
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.println();
+		}
+		*/
 		return neighbors;
 	}
-	
+
 	public int[] transposition (int[] array, int i, int j) {
-		int temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-		return array;
+		int[] copy = Arrays.copyOf(array, array.length);
+		int temp = copy[i];
+		copy[i] = copy[j];
+		copy[j] = temp;
+		return copy;
 	}
+
 }
--- lab10/BreadthFirstSolverTest.java
+++ lab10/BreadthFirstSolverTest.java
@@ -10,7 +10,13 @@ public class BreadthFirstSolverTest {
 	@Before
 	public void setUp() throws Exception {
 	}
-
+/*	
+	@Test
+	public void test() {
+		 sbs.solve(new int[] { 1,2,0,3,4,5,6,7,8 },new int[] { 1,0,2,3,4,5,6,7,8 },10);
+	}
+*/
+	
 	@Test
 	public void testNeighbors() {
 		for (int[] e:sbs.neighbors(new int[] { 1,2,0,3,4,5,6,7,8 })) {
@@ -19,10 +25,18 @@ public class BreadthFirstSolverTest {
 			}
 			System.out.println();
 		}
+		/*
 		int a[] = sbs.transposition(new int[] { 1,2,0,3,4,5,6,7,8 }, 0, 3);
 		for (int i = 0 ; i < a.length ; i++) {
 			System.out.print(a[i]+" ");
 		}
-	}
+		System.out.println();
 
+		int b[] = sbs.solve(new int[] { 1,2,0,3,4,5,6,7,8 },new int[] { 1,2,0,3,4,5,6,7,8 },10);
+		//for (int i = 0 ; i < b.length ; i++) {
+		//	System.out.print(b[i]+" ");
+		//}
+		System.out.println(b.length);
+		 */
+	}
 }
