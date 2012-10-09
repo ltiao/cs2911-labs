6c8dc093aa68066024e37585a141a08f4358e40c
Louis Tiao <louistiao@gmail.com>
2012-10-09 16:08:33 +1100

    good shit

--- lab10/SlidingBlockSolverTest.java
+++ lab10/SlidingBlockSolverTest.java
@@ -12,7 +12,7 @@ public abstract class SlidingBlockSolverTest {
 	@Before
 	public abstract void setUp();
 	
-	//@Test
+	@Test
 	public void test4by4_1() {
 		System.out.println(Arrays.toString(sbs.solve(new int[] { 1, 5, 2, 3, 4, 6, 7, 11, 8, 9, 10, 0, 12, 13, 14, 15 }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, 100000)));		
 	}
--- lab10/iteratedDepthFirstSolver.java
+++ lab10/iteratedDepthFirstSolver.java
@@ -9,15 +9,15 @@ public class iteratedDepthFirstSolver implements SlidingBlockSolver {
 	public int[] solve(int[] start, int[] goal, int maxMoves) {
 		PuzzleConfiguration startConf = new PuzzleConfiguration(start);
 		PuzzleConfiguration goalConf = new PuzzleConfiguration(goal);
-		Stack <PuzzleConfiguration> queue = new Stack<PuzzleConfiguration>();
+		Stack <PuzzleConfiguration> stack = new Stack<PuzzleConfiguration>();
 		HashMap <PuzzleConfiguration,Boolean> marker = new HashMap<PuzzleConfiguration,Boolean>();
 
 		// enqueue start configuration onto queue
-		queue.add(startConf);
+		stack.push(startConf);
 		// mark start configuration
 		marker.put(startConf, true);
-		while (!queue.isEmpty()) {
-			PuzzleConfiguration t = queue.pop();
+		while (!stack.isEmpty()) {
+			PuzzleConfiguration t = stack.pop();
 			System.out.println(t);
 			if (t.equals(goalConf)) {
 				if (t.moves.size() <= maxMoves) {
@@ -30,7 +30,7 @@ public class iteratedDepthFirstSolver implements SlidingBlockSolver {
 				PuzzleConfiguration o = t.opposite(e);
 				if (!marker.containsKey(o)) {
 					marker.put(o, true);
-					queue.push(o);
+					stack.push(o);
 				}
 			}
 		}
